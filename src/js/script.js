cd07ac3e6f38d8ece32a6bd5a5f0a5d47e28f855
Diana Yamtsova <dianayamtsova@mail.ru>
2019-10-25 18:44:29 +0300

    Vanilla main script

--- src/js/script.js
+++ src/js/script.js
@@ -1,53 +1,76 @@
 
+document.addEventListener("DOMContentLoaded", function () {
 
-$(document).ready(function () {
-
-	$(function() {
-	  $.scrollify({
-	    section : ".scroll-section",
-	    setHeights: true,
-	    sectionName: false
-	  });
-	});
-
+  $.scrollify({
+    section : ".scroll-section",
+    setHeights: true,
+    sectionName: false
+  });
 
 	// по клику на кнопку "Заказать" открывается модальное окно
+  let orderBtns = document.querySelectorAll(".order-btn");
 
-  $('.order-btn').on('click', function () {
-      $('.modal')
-          .fadeIn()
-      ;
+  for (let i = 0; i < orderBtns.length; i++) {
 
-      $('.body').addClass('modal-opened');
-  });
+    let btn = orderBtns[i];
 
-  var videoSrc = $('.video__file').attr("src");
-  $('.video__file').attr("src","");
+    btn.addEventListener('click', function(e) {
+
+      let orderModalCls = document.getElementById("order-modal").classList;
+      orderModalCls.add('visible');
+      orderModalCls.add('fadeIn'); // IE не поддерживает добаление нескольких классов (classList.add('visible', 'fadeIn'))
+      document.getElementById("body").classList.add('modal-opened');
+
+    });
+
+  }
+
+  // запоминаем src видео, чтобы после закрытия src стереть и видео выключилось
+  let video = document.getElementById("video");
+  let videoFile = video.querySelector('.video__file');
+  let videoSrc = videoFile.getAttribute('src');
+  videoFile.src = "";
 
   // по клику на кнопку "Видео" открывается видео
+  let videoBtns = document.querySelectorAll(".video-btn");
 
-  $('.video-btn').on('click', function () {
-      $('.video')
-        .fadeIn()
-        .find('.video__file')
-        .attr("src", videoSrc)
-      ;
+  for (let i = 0; i < videoBtns.length; i++) {
 
-      $('.body').addClass('modal-opened');
-  });
+    let btn = videoBtns[i];
 
+    btn.addEventListener('click', function(e) {
+
+      video.classList.add('visible');
+      video.classList.add('fadeIn');
+      videoFile.setAttribute('src', videoSrc);
+
+      document.getElementById("body").classList.add('modal-opened');
+
+    });
+
+  }
 
   // по клику на крестик закрывается видео
+  document.querySelector(".video__close").addEventListener('click', function(e) {
 
-  $('.video__close').on('click', function () {
-      $(this)
-        .closest('.video')
-        .fadeOut()
-        .find('.video__file')
-        .attr("src", "")
-      ;
+    let close = this;
+    let closeParent = close.parentNode;
 
-      $('.body').removeClass('modal-opened');
+    //- ищем ближайшего родителя с классом video
+    while (!closeParent.classList.contains('video')) {
+      closeParent = closeParent.parentNode;
+    }
+
+    let video = closeParent;
+    video.classList.remove('fadeIn');
+
+    setTimeout(function() {
+      video.classList.remove('visible');
+      video.querySelector('.video__file').setAttribute('src', '');
+    }, 300);
+
+    $('.body').removeClass('modal-opened');
+
   });
   
 });

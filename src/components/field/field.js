e8d359d7813c03fc695d51db73a8b63c09903bc0
Diana Yamtsova <dianayamtsova@mail.ru>
2019-11-03 16:05:03 +0300

    Changed type validation to name validation and replaced if-else to switch-case

--- src/components/field/field.js
+++ src/components/field/field.js
@@ -2,7 +2,7 @@
 document.addEventListener("DOMContentLoaded", function () {
 
 	//- подключаем к полям для телефонов маску ввода
-	let phoneInputs = document.querySelectorAll(".field__input[type='phone']");
+	let phoneInputs = document.querySelectorAll(".field__input[name='phone']");
 
 	Array.prototype.forEach.call(phoneInputs, el => {
 
@@ -29,46 +29,36 @@ document.addEventListener("DOMContentLoaded", function () {
 			}
 
 			let field = elemParent;
-			let type = elem.getAttribute('type');
+			let name = elem.getAttribute('name');
 			let val = elem.value;
 			let nameRegexp = "[а-яА-Я ]{2,20}$";
 			let phoneRegexp = "[+]{1} [0-9]{1} [(]{1}[0-9]{3}[)]{1} [0-9]{3}[-]{1}[0-9]{2}[-]{1}[0-9]{2}";
 
-			if (type === 'text') {
+			switch(name) {
+			  case 'name':
+			  	if (val.match(nameRegexp) !== null) {
+						field.classList.remove('field_incorrect');
+						field.classList.add('field_correct');
+					}
 
-				if (val.match(nameRegexp) !== null) {
+					else {
+						field.classList.remove('field_correct');
+						field.classList.add('field_incorrect');
+					}
+			    break;
 
-					field.classList.remove('field_incorrect');
-					field.classList.add('field_correct');
+			  case 'phone':
+				  if (val.match(phoneRegexp) !== null) {
+						field.classList.remove('field_incorrect');
+						field.classList.add('field_correct');
+					}
 
-				}
-
-				else {
-
-					field.classList.remove('field_correct');
-					field.classList.add('field_incorrect');
-
-				}
-
-			}
-
-			else if (type === 'phone') {
-
-				if (val.match(phoneRegexp) !== null) {
-
-					field.classList.remove('field_incorrect');
-					field.classList.add('field_correct');
-
-				}
-
-				else {
-
-					field.classList.remove('field_correct');
-					field.classList.add('field_incorrect');
-
-				}
-
-			}
+					else {
+						field.classList.remove('field_correct');
+						field.classList.add('field_incorrect');
+					}
+			    break;
+	    }
 
 			let event;
 
